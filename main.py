dc3f9fa53a3e8aa2eb96efdf6575753de89f3039
XJwangdage <114815506+XJwangdage@users.noreply.github.com>
2023-10-14 14:56:31 +0800

    Rename main.js to main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import re
 while(True):
-    id = re.search(r'\d+', input("请输入作品ID或链接 > "))
+    id = re.search(r'\d+'， input("请输入作品ID或链接 > "))
     if id:
         id = id.group()
         print(f'社区链:\thttps://shequ.codemao.cn/work/{id}\nH5链:\thttps://coco.codemao.cn/editor/player/{id}?channel=h5\n社区iframe链:\thttps://coco.codemao.cn/editor/player/{id}?channel=community\n编辑器链:\thttps://coco.codemao.cn/editor/?workId={id}')
